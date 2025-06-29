765603034714008f076c0a045fd06807dce83f5c
dpl <dpl0@users.noreply.github.com>
2025-06-29 21:50:47 +0200

    chore: impl ram
    Signed-off-by: dpl <dpl0@users.noreply.github.com>

--- src/gameboy/ram.rs
+++ src/gameboy/ram.rs
@@ -1,50 +1,40 @@
-/*
- * General memory map:
- *
- * Interrupt Enable Register
- * --------------------------- 0xFFFF
- * Internal RAM
- * --------------------------- 0xFF80
- * Empty but unusable for I/O
- * --------------------------- 0xFF4C
- * I/O ports
- * --------------------------- 0xFF00
- * Empty but unusable for I/O
- * --------------------------- 0xFEA0
- * Sprite Attrib Memory (OAM)
- * --------------------------- 0xFE00
- * Echo of 8kB Internal RAM
- * --------------------------- 0xE000
- * 8kB Internal RAM
- * --------------------------- 0xC000
- * 8kB switchable RAM bank
- * --------------------------- 0xA000
- * 8kB Video RAM
- * --------------------------- 0x8000 --
- * 16kB switchable ROM bank             |
- * --------------------------- 0x4000   |= 32kB Cartridge
- * 16kB ROM bank #0                     |
- * --------------------------- 0x0000 --
- * * b = bit, B = byte
- *
- * Writing at E000-FE00 writes at C000-DE00
- * Writing at C000-DE00 writes at C000-DE00
- *
- */
 
+/// This module implements the memory for GemuB.
+///
+/// The Game Boy has a 16-bit address bus, which is used to address ROM, RAM, and I/O.
+///
+///  This is a map of the address space:
+///
+///  0000 - 3FFF | 16 KiB ROM bank 00             | Cartridge, usually a fixed bank
+///  4000 - 7FFF | 16 KiB ROM Bank 01–NN          | Cartridge, switchable bank
+///  8000 - 9FFF | 8 KiB Video RAM (VRAM)         | In CGB mode, switchable bank 0/1
+///  A000 - BFFF | 8 KiB External RAM             | Cartridge, switchable bank
+///  C000 - CFFF | 4 KiB Work RAM (WRAM)          |
+///  D000 - DFFF | 4 KiB Work RAM (WRAM)          | In CGB mode, switchable bank 1–7
+///  E000 - FDFF | Echo RAM (mirror of C000–DDFF) | Forbidden by Nintendo.
+///  FE00 - FE9F | Object attribute memory (OAM)  |
+///  FEA0 - FEFF | Not Usable                     | Forbidden by Nintendo.
+///  FF00 - FF7F | I/O Registers                  |
+///  FF80 - FFFE | High RAM (HRAM)                |
+///  FFFF - FFFF | Interrupt Enable register (IE) |
+///
+///  ## Echo RAM
+///  Accesses to E000-FDFF go to C000-DDFF.
 use std::fmt;
 
 use crate::gameboy::rom::Rom;
 
-const MEMORY_SIZE: usize = 64 * 1024;
-const START_ROM_BANK: usize = 0x0000;
+/// Represents the size of the memory in bytes.
+pub const MEMORY_SIZE: usize = 64 * 1024;
 
 /// Represents the mirroring secton of the memory.
-const MIRRORING_RANGE: std::ops::RangeInclusive<usize> = 0xE000..=0xFE00;
-const MIRRORING_OFFSET: usize = 0xE000 - 0xC000;
+pub const MIRRORING_RANGE: std::ops::RangeInclusive<usize> = 0xE000..=0xFDFF;
+/// Represents the offset used when accessing the mirrored section of the memory.
+pub const MIRRORING_OFFSET: usize = 0xE000 - 0xC000;
 
+/// Represents the whole memory space of the Game Boy.
 pub(crate) struct Memory {
-    pub ram: [u8; MEMORY_SIZE],
+    ram: [u8; MEMORY_SIZE],
 }
 
 impl Memory {
@@ -57,6 +47,8 @@ impl Memory {
 
     /// Creates memory and load given rom.
     pub fn new_with_rom(rom: Rom) -> Memory {
+        const START_ROM_BANK: u16 = 0x0000;
+
         let mut memory = Self::new();
         let bytes_written = memory
             .write_slice(START_ROM_BANK, &rom.bytecode)
@@ -66,36 +58,42 @@ impl Memory {
         memory
     }
 
+    /// Helper method to calculate the memory location considering mirroring addresses.
+    fn physical_address(&self, location: u16) -> usize {
+        // Accesses to E000-FDFF go to C000-DDFF.
+        if MIRRORING_RANGE.contains(&(location as usize)) {
+            location as usize - MIRRORING_OFFSET
+        } else {
+            location as usize
+        }
+    }
+
+    /// Reads a byte from the memory at the specified location.
+    pub fn read(&self, location: u16) -> u8 {
+        self.ram[self.physical_address(location)]
+    }
+
     /// Writes a slice of data to the memory at the specified location.
     /// Mostly used for writing ROM data.
-    pub fn write_slice(
-        &mut self,
-        location: usize,
-        data: &[u8],
-    ) -> anyhow::Result<usize, WriteError> {
-        if location >= MEMORY_SIZE {
-            return Err(WriteError::OutOfBounds);
-        }
+    ///
+    /// Writes that target both the mirroring (E000-FDFF) and non-mirroring ranges will fail.
+    pub fn write_slice(&mut self, location: u16, data: &[u8]) -> Result<usize, MemAccessError> {
+        let location = location as usize;
 
-        let end = location
-            .checked_add(data.len())
-            .unwrap_or(MEMORY_SIZE)
-            .min(MEMORY_SIZE);
-        if end > MEMORY_SIZE {
-            return Err(WriteError::SliceEndOutOfBounds);
-        }
+        let Some(end) = location.checked_add(data.len()) else {
+            return Err(MemAccessError::SliceEndOutOfBounds);
+        };
+        let end = end.min(MEMORY_SIZE);
 
-        // Writing at E000-FE00 writes at C000-DE00
-        // If the location and the end are in the mirroring range, just call write_slice with
-        // adjusted location.
+        // Accesses to E000-FDFF go to C000-DDFF.
+        // Writing exclusively in the mirroring range.
         if MIRRORING_RANGE.contains(&location) && MIRRORING_RANGE.contains(&end) {
-            return Self::write_slice(self, location - MIRRORING_OFFSET, data);
+            return Self::write_slice(self, (location - MIRRORING_OFFSET) as u16, data);
         }
 
-        // If only a part of the slice is in the mirroring range, return an error. This is not
-        // really expected to happen.
+        // Writing partially in the mirroring range is not supported.
         if MIRRORING_RANGE.contains(&location) || MIRRORING_RANGE.contains(&end) {
-            return Err(WriteError::SliceInMirroringRange);
+            return Err(MemAccessError::SliceInMirroringRange);
         }
 
         let count = end.saturating_sub(location);
@@ -104,19 +102,20 @@ impl Memory {
     }
 
     /// Writes a single byte to the memory at the specified location.
-    pub fn write_byte(&mut self, location: usize, byte: u8) -> anyhow::Result<(), WriteError> {
-        if location >= MEMORY_SIZE {
-            return Err(WriteError::OutOfBounds);
-        }
+    ///
+    /// Accesses to E000-FDFF go to C000-DDFF.
+    pub fn write_byte(&mut self, location: u16, byte: u8) {
+        self.ram[self.physical_address(location)] = byte;
+    }
 
-        // Writing at E000-FE00 writes at C000-DE00
-        if MIRRORING_RANGE.contains(&location) {
-            self.ram[location - MIRRORING_OFFSET] = byte;
-        } else {
-            self.ram[location] = byte;
-        }
+    /// Reads directly from the memory, bypassing mirrorring logic.
+    pub fn read_raw(&self, location: u16) -> u8 {
+        self.ram[location as usize]
+    }
 
-        Ok(())
+    /// Writes byte directly from the memory, bypassing mirrorring logic.
+    pub fn write_byte_raw(&mut self, location: u16, byte: u8) {
+        self.ram[location as usize] = byte;
     }
 }
 
@@ -126,19 +125,48 @@ impl fmt::Debug for Memory {
     }
 }
 
+/// Index into the memory.
+/// This will panic if the index is out of bounds.
+impl std::ops::Index<usize> for Memory {
+    type Output = u8;
+
+    fn index(&self, index: usize) -> &Self::Output {
+        if index >= MEMORY_SIZE {
+            panic!("Index out of bounds");
+        }
+
+        // Accesses to E000-FDFF go to C000-DDFF.
+        &self.ram[self.physical_address(index as u16)]
+    }
+}
+
+/// Mutably index into the memory.
+/// This will panic if the index is out of bounds.
+impl std::ops::IndexMut<usize> for Memory {
+    fn index_mut(&mut self, index: usize) -> &mut Self::Output {
+        if index >= MEMORY_SIZE {
+            panic!("Index out of bounds");
+        }
+
+        // Accesses to E000-FDFF go to C000-DDFF.
+        &mut self.ram[self.physical_address(index as u16)]
+    }
+}
+
 #[derive(Debug)]
-pub enum WriteError {
-    OutOfBounds,
+/// Represents errors that can occur when accessing memory.
+pub enum MemAccessError {
     SliceEndOutOfBounds,
     SliceInMirroringRange,
 }
 
-impl fmt::Display for WriteError {
+impl fmt::Display for MemAccessError {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         match self {
-            WriteError::OutOfBounds => write!(f, "Write operation out of bounds"),
-            WriteError::SliceEndOutOfBounds => write!(f, "Slice end exceeds memory bounds"),
-            WriteError::SliceInMirroringRange => write!(f, "Slice overlaps with mirroring range"),
+            MemAccessError::SliceEndOutOfBounds => write!(f, "Slice end exceeds addressable space"),
+            MemAccessError::SliceInMirroringRange => {
+                write!(f, "Slice overlaps with mirroring range")
+            }
         }
     }
 }
@@ -146,6 +174,52 @@ impl fmt::Display for WriteError {
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    #[test]
+    fn test_read_non_mirroring() {
+        let mut memory = Memory::new();
+        memory.ram[0x0000] = 42; // Set a value to read
+        let byte = memory.read(0x0000);
+        assert_eq!(byte, 42);
+
+        let byte = memory[MEMORY_SIZE - 1];
+        assert_eq!(byte, 0); // Default value is 0
+    }
+
+    #[test]
+    fn test_read_mirroring() {
+        let mut memory = Memory::new();
+        // Accesses to E000-FDFF go to C000-DDFF.
+        // 1 byte before the mirroring range
+        memory[0xDFFF] = 1;
+        assert_eq!(memory[0xDFFF], 1);
+        assert_eq!(memory[0xCFFF], 0); // Should not affect CFFF
+
+        // Start of the mirroring range, will write to C000 instead.
+        memory[0xE000] = 2; // mirorred
+        assert_eq!(memory[0xE000], 2); // Reads from C000
+        assert_eq!(memory[0xC000], 2);
+
+        // Start+1 mirroring range, will write to C001 instead.
+        memory[0xE001] = 3; // mirorred
+        assert_eq!(memory[0xE001], 3); // Reads from C001
+        assert_eq!(memory[0xC001], 3);
+
+        // End-1 of mirroring range, will write to DDFE instead.
+        memory[0xFDFE] = 4; // mirorred
+        assert_eq!(memory[0xFDFE], 4); // Should read from DDFE
+        assert_eq!(memory[0xDDFE], 4);
+
+        // End of the mirroring range, will write to DDFF instead.
+        memory[0xFDFF] = 5; // mirorred
+        assert_eq!(memory[0xFDFF], 5); // Should read from DDFF
+        assert_eq!(memory[0xDDFF], 5);
+
+        // End+1 mirroring range, will write to FE00 instead.
+        memory[0xFE00] = 6; // non-mirorred
+        assert_eq!(memory[0xFE00], 6); // Should read from DE00
+        assert_eq!(memory[0xDE00], 0);
+    }
 
     #[test]
     fn test_write_slice_basic() {
@@ -161,7 +235,7 @@ mod tests {
         let mut mem = Memory::new();
         let data = [1, 2, 3, 4];
         let start = MEMORY_SIZE - 2;
-        let written = mem.write_slice(start, &data).unwrap();
+        let written = mem.write_slice(start as u16, &data).unwrap();
         assert_eq!(written, 2);
         assert_eq!(&mem.ram[start..MEMORY_SIZE], &data[..2]);
     }
@@ -178,7 +252,7 @@ mod tests {
     fn test_write_slice_inside_mirroring() {
         let mut memory = Memory::new();
         let data = [1, 2, 3, 4, 5];
-        // Writing at E000-FE00 writes at C000-DE00
+        // Accesses to E000-FDFF go to C000-DDFF.
         let bytes_written = memory.write_slice(0xE000, &data).unwrap();
         assert_eq!(bytes_written, data.len());
         assert_eq!(&memory.ram[0xC000..0xC005], &data);
@@ -189,44 +263,46 @@ mod tests {
     fn test_write_slice_partial_overlap_mirroring() {
         let mut memory = Memory::new();
         let data = [1, 2, 3, 4, 5];
-        // This should fail because it overlaps with the mirroring range
-        // Writing at E000-FE00 writes at C000-DE00
+        // Accesses to E000-FDFF go to C000-DDFF.
+        // This should fail because it partially overlaps with the mirroring range
         let result = memory.write_slice(0xFDFF, &data);
-        assert!(matches!(result, Err(WriteError::SliceInMirroringRange)));
+        assert!(matches!(result, Err(MemAccessError::SliceInMirroringRange)));
     }
 
     #[test]
     fn test_write_byte_basic() {
         let mut memory = Memory::new();
         let byte = 0x42;
-        memory.write_byte(0x0000, byte).unwrap();
+        memory[0x0000] = byte;
         assert_eq!(memory.ram[0x0000], byte);
     }
 
     #[test]
+    #[should_panic(expected = "Index out of bounds")]
     fn test_write_byte_out_of_bounds() {
         let mut memory = Memory::new();
-        let result = memory.write_byte(MEMORY_SIZE, 0x42);
-        assert!(matches!(result, Err(WriteError::OutOfBounds)));
+        // Since write_byte cannot fail, we can only test IndexMut.
+        memory[MEMORY_SIZE] = 0x42;
     }
 
     #[test]
     fn test_write_byte_e000_to_fe00() {
         let mut memory = Memory::new();
         let byte = 0x42;
-        // Writing at E000-FE00 writes at C000-DE00
-        memory.write_byte(0xE000, byte).unwrap();
-        assert_eq!(memory.ram[0xC000], byte); // Should write to C000
-        assert_eq!(memory.ram[0xE000], 0x00); // Should write to E000 as well
+        // Accesses to E000-FDFF go to C000-DDFF.
+        memory.write_byte(0xE000, byte);
+        assert_eq!(memory[0xC000], byte); // Should write to 0xC000
+        assert_eq!(memory[0xE000], byte); // And to 0xE000
     }
 
     #[test]
     fn test_write_byte_c000_to_de00() {
         let mut memory = Memory::new();
         let byte = 0x42;
-        // Writing at C000-DE00 writes at C000-DE00
-        memory.write_byte(0xC000, byte).unwrap();
-        assert_eq!(memory.ram[0xC000], byte); // Should write to C000
-        assert_eq!(memory.ram[0xE000], 0x00); // Should not affect E000
+        // Accesses to E000-FDFF go to C000-DDFF.
+        memory[0xC000] = byte;
+        assert_eq!(memory.ram[0xC000], byte); // Should write to 0xC000
+        assert_eq!(memory.ram[0xE000], 0x00); // Should not affect 0xE000
     }
 }
+
