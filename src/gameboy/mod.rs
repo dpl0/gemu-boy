645bd193ecef6f994af098934621c2d3f8563eef
dpl <dpl0@users.noreply.github.com>
2025-07-08 21:47:03 +0200

    chore: use path references

--- src/gameboy/mod.rs
+++ src/gameboy/mod.rs
@@ -9,17 +9,19 @@ mod screen;
 /// The main GameBoy structure that holds everything needed to run the emulator.
 #[derive(Debug)]
 pub struct GameBoy {
+    pub rom_name: Utf8PathBuf,
     pub cpu: cpu::Cpu,
     pub ram: ram::Memory,
     pub screen: screen::Screen,
 }
 
 impl GameBoy {
-    pub fn new(rom_name: Utf8PathBuf) -> anyhow::Result<GameBoy> {
-        let rom = rom::Rom::new(rom_name)?;
     /// Creates a new instance of the GameBoy emulator with the given ROM file path.
+    pub(crate) fn new(rom_name: Utf8PathBuf) -> anyhow::Result<GameBoy> {
+        let rom = rom::Rom::new(&rom_name)?;
 
         Ok(GameBoy {
+            rom_name,
             cpu: cpu::Cpu::new(),
             ram: ram::Memory::new_with_rom(rom),
             screen: screen::Screen::new(),
